3dcbed546b53ba8ba2d0f8f0938a68522d888d20
rafi nizar <rafi.nizar15@gmail.com>
2021-02-28 22:04:35 +0700

    added: economy and levels

---
+++ commands/economy/pay.js
@@ -0,0 +1,49 @@
+const economy = require('../../economy')
+const { bot_channels } = require('../../config.json')
+
+module.exports = {
+    commands: ['pay',],
+    minArgs: 2,
+    maxArgs: 2,
+    expectedArgs: "<Target user's @> <Amount of coins>",
+    description: "Displays a user's coins.",
+    callback: async (message, arguments, text) => {
+        const { guild, member } = message
+
+        const target = message.mentions.users.first()
+        if (!target) {
+            // message.reply(`Please specify someone to give coin to`)
+            message.client.channels.cache.get(bot_channels[guildId]).send(`Please specify someone to give coin to`)
+            return
+        }
+
+        const coinsToGive = arguments [1]
+        if (isNaN(coinsToGive)) {
+            // message.reply(`Please provide a valid number of coins to give.`)
+            message.client.channels.cache.get(bot_channels[guildId]).send(`Please provide a valid number of coins to give.`)
+            return
+        }
+
+        const coinsOwned = await economy.getCoins(guild.id, member.id)
+        if (coinsOwned < coinsToGive) {
+            // message.reply(`You do not have ${coinsToGive} coins!.`)
+            message.client.channels.cache.get(bot_channels[guildId]).send(`You do not have ${coinsToGive} coins!.`)
+            return
+        }
+
+        const remainingCoins = await economy.addCoins(
+            guild.id,
+            member.id,
+            coinsToGive * -1
+        )
+
+        const newBalance = await economy.addCoins(
+            guild.id,
+            target.id,
+            coinsToGive
+        )
+
+        // message.reply(`You have given <@${target.id}> ${coinsToGive} coins.`)
+        message.client.channels.cache.get(bot_channels[guild.id]).send(`You have given <@${target.id}> ${coinsToGive} coins.`)
+    }
+}
---
+++ economy.js
@@ -0,0 +1,78 @@
+const mongo = require('./mongo')
+
+const profileSchema = require('./schemas/profile-schema')
+
+const coinsCache = {}
+
+module.exports = (client) => {}
+
+module.exports.addCoins = async (guildId, userId, coins) => {
+    return await mongo().then(async (mongoose) => {
+        try {
+            // console.log('running findOneAndUpdate()')
+
+            const result = await profileSchema.findOneAndUpdate({
+                guildId,
+                userId
+            }, {
+                guildId,
+                userId,
+                $inc: {
+                    coins
+                }
+            }, {
+                upsert: true,
+                new: true
+            })
+            // console.log(result)
+
+            coinsCache[`${guildId}-${userId}`] = result.coins
+
+            return result.coins
+        } finally {
+            mongoose.connection.close()
+        }
+    })
+}
+
+module.exports.getCoins = async (guildId, userId) => {
+
+    const cachedValue = coinsCache[`${guildId}-${userId}`]
+
+    if (cachedValue) {
+        return cachedValue
+    }
+
+    return await mongo().then(async mongoose => {
+        try {
+            // console.log('running findOne()')
+
+            const result = await profileSchema.findOne({
+                guildId,
+                userId
+            })
+
+            console.log(result)
+
+            let coins = 0
+
+            if (result) {
+                coins = result.coins
+            } else {
+                // console.log('inserting a doc')
+                await new profileSchema({
+                    guildId,
+                    userId,
+                    coins
+                }).save()
+            }
+
+            coinsCache[`${guildId}-${userId}`] = coins
+
+            return coins
+
+        } finally {
+            mongoose.connection.close()
+        }
+    })
+}
