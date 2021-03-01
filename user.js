97956daa92c015f29972698bb85c2bb5b89b6a3a
rafi nizar <rafi.nizar15@gmail.com>
2021-03-01 13:08:27 +0700

    added: profile command

---
+++ user.js
@@ -0,0 +1,18 @@
+const mongo = require('./mongo')
+
+const profileSchema = require('./schemas/profile-schema')
+
+module.exports.getProfile = async (guildId, userId) => {
+    return await mongo().then(async (mongoose) => {
+        try {
+            const result = await profileSchema.findOne({
+                guildId,
+                userId
+            })
+
+            return result
+        } finally {
+            mongoose.connection.close()
+        }
+    })
+}
