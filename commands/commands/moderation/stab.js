c131bab5a91e73d6ed8999eb8085d92b5968a876
Rafi Nizar <rafi.nizar15@gmail.com>
2021-08-26 21:12:17 +0700

    added: stab

---
+++ commands/commands/moderation/stab.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const user = require('@util/user')
+const { bot_channels } = require('@root/config.json')
+
+module.exports = {
+    commands: ['stab'],
+    permissionError: 'NO.',
+    permissions: 'ADMINISTRATOR',
+    description: 'Kick a user.',
+    callback: async (message, arguments) => {
+        const { guild, member } = message
+        const guildId = guild.id
+        const memberId = member.id
+
+        const tag = `<@${memberId}>`
+
+        // temp, lupa cara pake base command
+        if (!member.hasPermission('KICK_MEMBERS')) {
+            message.reply('No permission to use command.')
+        }
+
+        const mention = message.mentions.users.first()
+        if (!mention) {
+            message.reply('Tag a user.')
+        } else {
+            const target = message.guild.members.cache.get(mention.id)
+            target.kick()
+            message.channel.send(`${tag} has been stabbed and is now bleeding to death.`)
+        }
+
+    }
+}
